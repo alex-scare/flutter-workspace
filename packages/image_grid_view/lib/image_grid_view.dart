dd235c6eec034d1967f39253d219047c6e4b0284
alexscare <al.pugachev96@gmail.com>
2023-02-17 00:01:11 +0400

    remove moved to separate package files

--- packages/image_grid_view/lib/image_grid_view.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library image_grid_view;
-
-export 'src/image_grid_view.dart';
-
-// TODO: Export any libraries intended for clients of this package.
